040271ab429394ed7e898ee8f0ce6b29df8e1ac3
zwbai <zwbai@bjtu.edu.cn>
2018-04-06 21:51:44 +0800

    test_1:两数相加

---
+++ LeetCode_Michael/LeetCode_Michael/test_1.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		int num = nums.size();
+		vector<int> sum;
+		for (int i = 0; i < num; i++) {
+			for (int j = i + 1; j < num; j++) {
+				if (nums[i] + nums[j] == target) {
+					sum.push_back(i);
+					sum.push_back(j);
+				}
+			}
+		}
+		return sum;
+	}
+};
