4d7178b4b63a0d32edcfb30e02b2c018e42281c2
zwbai <zwbai@bjtu.edu.cn>
2018-04-07 21:13:54 +0800

    test3/7

---
+++ LeetCode_Michael/LeetCode_Michael/test_3.cpp
@@ -0,0 +1,62 @@
+#include <string>
+#include <math.h>
+#include <assert.h>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+using namespace std;
+
+class Solution {
+public:
+	int lengthOfLongestSubstring(string s) {
+		vector<int> dict(256, -1);
+		int maxLen = 0, start = -1;
+		for (int i = 0; i != s.length(); i++) {
+			if (dict[s[i]] > start)
+				start = dict[s[i]];
+			dict[s[i]] = i;
+			maxLen = max(maxLen, i - start);
+		}
+		return maxLen;
+	}
+};
+
+string stringToString(string input) {
+	assert(input.length() >= 2);
+	string result;
+	for (int i = 0; i < input.length(); i++) {
+		char currentChar = input[i];
+		if (input[i] == '\\') {
+			char nextChar = input[i + 1];
+			switch (nextChar) {
+			case '\"': result.push_back('\"'); break;
+			case '/': result.push_back('/'); break;
+			case '\\': result.push_back('\\'); break;
+			case 'b': result.push_back('\b'); break;
+			case 'f': result.push_back('\f'); break;
+			case 'r': result.push_back('\r'); break;
+			case 'n': result.push_back('\n'); break;
+			case 't': result.push_back('\t'); break;
+			default: break;
+			}
+			i++;
+		}
+		else {
+			result.push_back(currentChar);
+		}
+	}
+	return result;
+}
+// 
+// int main() {
+// 	string line;
+// 	while (getline(cin, line)) {
+// 		string s = stringToString(line);
+// 
+// 		int ret = Solution().lengthOfLongestSubstring(s);
+// 
+// 		string out = to_string(ret);
+// 		cout << out << endl;
+// 	}
+// }
